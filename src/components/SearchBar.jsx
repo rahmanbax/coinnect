076b25af8b3c07c7abd0a9006751ecf9a15ec777
rahmanbax <basyirahman@gmail.com>
2025-01-25 17:20:57 +0700

    Update component: SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div className='flex items-center gap-2 p-2 bg-[#F5F5F5] rounded-xl w-fit'>
+    <div className='flex items-center gap-2 p-2 bg-[#F5F5F5] rounded-xl w-[208px]'>
       <MagnifyingGlass className='opacity-50' size={16} weight="bold" />
       <input type="text" className='focus:outline-none text-sm font-medium' placeholder='Search'>
       </input>
