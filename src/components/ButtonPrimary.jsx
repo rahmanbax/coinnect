0eb8e37587a95b3e5df8ebd342c96dd180f29eca
rahmanbax <basyirahman@gmail.com>
2025-01-25 15:57:32 +0700

    Update font: import url Sora

--- src/components/ButtonPrimary.jsx
+++ src/components/ButtonPrimary.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonPrimary = (props) => {
     return (
-        <button className="rounded-xl bg-[#FFC31B] text-black font-bold py-2 px-4">
+        <button className="rounded-xl bg-[#FFC31B] text-black font-medium py-2 px-4">
             {props.children}
         </button>
     );
--- src/components/ButtonSecondary.jsx
+++ src/components/ButtonSecondary.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonSecondary = (props) => {
     return (
-        <button className="rounded-xl bg-white text-black font-bold py-2 px-4">
+        <button className="rounded-xl bg-white text-black font-medium py-2 px-4">
             {props.children}
         </button>
     );
