b85457efa98b1564c1cc1b823b3a477363d8318f
rahmanbax <basyirahman@gmail.com>
2025-01-25 15:42:51 +0700

    Create components: SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { CaretDown } from "@phosphor-icons/react";
 import ButtonPrimary from "./components/ButtonPrimary";
 import ButtonSecondary from "./components/ButtonSecondary";
+import CoinnectLogo from "./assets/coinnect-logo.png";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
-      <div className="justify-center flex flex-col min-h-screen items-center">
-        <h1>Test2</h1>
-        <ButtonPrimary>Sign Up</ButtonPrimary>
-        <ButtonSecondary>Login</ButtonSecondary>
+      <div className="">
+        <header>
+          <nav>
+            <div className="flex gap-10 items-center">
+              <a href="">
+                <img src={CoinnectLogo} alt="" />
+              </a>
+              <a href="#">Markets</a>
+              <a href="#">Swap</a>
+              <a href="#">Portfolio Tracker</a>
+              <div className="flex gap-1 items-center">
+                <p>More</p>
+                <CaretDown size={12} weight="bold" />
+              </div>
+            </div>
+            <div>
+              <SearchBar/>
+            </div>
+          </nav>
+        </header>
       </div>
     </>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import { MagnifyingGlass } from '@phosphor-icons/react'
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className='flex items-center gap-2 p-2 bg-[#F5F5F5] rounded-xl w-fit'>
+      <MagnifyingGlass className='opacity-50' size={16} weight="bold" />
+      <input type="text" className='focus:outline-none' placeholder='Search'>
+      </input>
+    </div>
+  )
+}
+
+export default SearchBar
