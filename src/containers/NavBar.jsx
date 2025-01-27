9db22d41abc61b4f436e678cf0f65fdab145347a
rahmanbax <basyirahman@gmail.com>
2025-01-28 01:00:01 +0700

    Update navbar with router

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { CaretDown } from "@phosphor-icons/react";
-import CoinnectLogo from "../assets/coinnect-logo.png";
+import CoinnectLogo from "../assets/coinnect-combination-logo.png";
 import SearchBar from "../components/SearchBar";
 import ButtonSecondary from "../components/ButtonSecondary";
 import ButtonPrimary from "../components/ButtonPrimary";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <header className="bg-white p-4">
       <nav className="flex justify-between items-center mx-14">
         <div className="flex gap-10 items-center">
-          <a href="">
-            <img src={CoinnectLogo} alt="" />
-          </a>
-          <a className="text-sm font-medium" href="#">
+          <Link to="/">
+            <img src={CoinnectLogo} alt="" className="w-[119px] h-[25px]" />
+          </Link>
+          <Link className="text-sm font-medium" to="/markets">
             Markets
-          </a>
-          <a className="text-sm font-medium" href="#">
+          </Link>
+          <Link className="text-sm font-medium" to="/swap">
             Swap
-          </a>
-          <a className="text-sm font-medium" href="#">
+          </Link>
+          <Link className="text-sm font-medium" to="/tracker">
             Portfolio Tracker
-          </a>
-          <div className="flex gap-1 items-center">
+          </Link>
+          {/* <div className="flex gap-1 items-center">
             <p className="text-sm font-medium">More</p>
             <CaretDown size={12} weight="bold" />
-          </div>
+          </div> */}
         </div>
         <div className="flex items-center gap-6">
           <SearchBar />
