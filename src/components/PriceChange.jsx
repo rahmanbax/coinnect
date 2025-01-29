c7768e238fdc08c3ff46d522072eec57374317f6
rahmanbax <basyirahman@gmail.com>
2025-01-29 23:00:54 +0700

    Update PriceChange styling and add Math function

--- src/components/PriceChange.jsx
+++ src/components/PriceChange.jsx
@@ -2,25 +2,31 @@ import { CaretDown, CaretUp } from "@phosphor-icons/react";
 import React from "react";
 
 const PriceChange = (props) => {
-    const {priceChange} = props;
+  const { priceChange } = props;
+
+  // Ambil nilai absolut dari priceChange
+  const absolutePriceChange = Math.abs(priceChange);
+
   return (
     <>
-      {priceChange < 0 ? (
+      {priceChange < 0 ? ( 
         <div className="p-5 w-[116px] flex items-center place-content-end">
-          <CaretDown size={16} weight="fill" className="text-red-500" />
-          <p className="text-base text-red-500 font-medium">{priceChange}%</p>
-        </div>
-      ) : priceChange === 0 ? (
-        <div className="p-5 w-[116px] text-end">
-          <p className="text-base text-gray-500 font-medium">
-            {priceChange}%
+          <CaretDown size={16} weight="fill" className="text-red-600" />
+          <p className="text-base text-red-600 font-medium">
+            {absolutePriceChange}%
           </p>
         </div>
-      ) : (
+      ) : priceChange === 0 ? ( 
+        <div className="p-5 w-[116px] text-end">
+          <p className="text-base text-gray-500 font-medium">
+            {absolutePriceChange}%
+          </p>
+        </div>
+      ) : ( 
         <div className="p-5 w-[116px] flex items-center place-content-end">
-          <CaretUp size={16} weight="fill" className="text-green-500" />
-          <p className="text-base text-green-500 font-medium">
-            {priceChange}%
+          <CaretUp size={16} weight="fill" className="text-green-600" />
+          <p className="text-base text-green-600 font-medium">
+            {absolutePriceChange}%
           </p>
         </div>
       )}
