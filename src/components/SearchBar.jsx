d283ccb6042bdd35951b3f1ae1a32ccd04fb4292
rahmanbax <basyirahman@gmail.com>
2025-02-07 21:06:31 +0700

    Refactor SearchBar component to use a button for improved accessibility and styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,13 @@
-import { MagnifyingGlass } from '@phosphor-icons/react'
-import React from 'react'
+import { MagnifyingGlass } from "@phosphor-icons/react";
+import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className='flex items-center gap-2 p-2 bg-[#F5F5F5] rounded-xl w-[208px]'>
-      <MagnifyingGlass className='opacity-50' size={16} weight="bold" />
-      <input type="text" className='focus:outline-none text-sm font-medium' placeholder='Search'>
-      </input>
-    </div>
-  )
-}
+    <button className="flex items-center gap-1 px-2 py-1 bg-[#F5F5F5] rounded-full stroke-1 border-1 border-gray-200">
+      <MagnifyingGlass className="opacity-50" size={12} weight="bold" />
+      <p className="font-normal text-sm text-gray-500">Search</p>
+    </button>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
