4073fddc42e03d358349211b2bfa5abc578f011b
rahmanbax <basyirahman@gmail.com>
2025-02-11 03:49:25 +0700

    Add PortfolioTracker component with NavBar

---
+++ src/pages/PortfolioTracker.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../containers/NavBar";
+
+const PortfolioTracker = () => {
+  return (
+    <>
+      <NavBar />
+      Tracker
+    </>
+  );
+};
+
+export default PortfolioTracker;
