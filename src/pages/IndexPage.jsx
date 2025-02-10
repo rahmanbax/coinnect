c6c58b6d2540d5a275d19cce6bde2655483dde8b
rahmanbax <basyirahman@gmail.com>
2025-02-11 03:49:21 +0700

    Add IndexPage component with NavBar and placeholder content

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../containers/NavBar";
+import Footer from "../containers/Footer";
+
+const IndexPage = () => {
+  return (
+    <div className="">
+      <NavBar />
+      <div className="">Index</div>
+    </div>
+  );
+};
+
+export default IndexPage;
