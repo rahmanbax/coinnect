6f072e0a687e61f0c85fc70e729c4accc7623da0
rahmanbax <basyirahman@gmail.com>
2025-02-11 03:48:16 +0700

    Add search modal functionality to NavBar component

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -1,41 +1,61 @@
-import React from "react";
-import { CaretDown } from "@phosphor-icons/react";
+import React, { useEffect, useRef, useState } from "react";
 import CoinnectLogo from "../assets/coinnect-combination-logo.png";
 import SearchBar from "../components/SearchBar";
 import ButtonSecondary from "../components/ButtonSecondary";
 import ButtonPrimary from "../components/ButtonPrimary";
 import { Link } from "react-router-dom";
+import Modal from "../components/Modal";
+import SearchModalLayout from "../components/SearchModal";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+      searchInputRef.current?.focus();
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
   return (
-    <header className="bg-white p-4">
-      <nav className="flex justify-between items-center mx-14">
-        <div className="flex gap-10 items-center">
-          <Link to="/">
-            <img src={CoinnectLogo} alt="" className="w-[119px] h-[25px]" />
-          </Link>
-          <Link className="text-sm font-medium" to="/markets">
-            Markets
-          </Link>
-          <Link className="text-sm font-medium" to="/swap">
-            Swap
-          </Link>
-          <Link className="text-sm font-medium" to="/tracker">
-            Portfolio Tracker
-          </Link>
-          {/* <div className="flex gap-1 items-center">
-            <p className="text-sm font-medium">More</p>
-            <CaretDown size={12} weight="bold" />
-          </div> */}
-        </div>
-        <div className="flex items-center gap-6">
-          <SearchBar />
-          <span className="text-sm opacity-20">|</span>
-          <ButtonSecondary>Login</ButtonSecondary>
-          <ButtonPrimary>Sign Up</ButtonPrimary>
-        </div>
-      </nav>
-    </header>
+    <>
+      <header className="bg-white p-4">
+        <nav className="flex justify-between items-center mx-14">
+          <div className="flex gap-10 items-center">
+            <Link to="/">
+              <img src={CoinnectLogo} alt="" className="w-[119px] h-[25px]" />
+            </Link>
+            <Link className="text-sm font-medium" to="/markets">
+              Markets
+            </Link>
+            <Link className="text-sm font-medium" to="/swap">
+              Swap
+            </Link>
+            <Link className="text-sm font-medium" to="/tracker">
+              Portfolio Tracker
+            </Link>
+          </div>
+          <div className="flex items-center gap-6">
+            <SearchBar onClick={() => setOpen(true)} />
+            <span className="text-sm opacity-20">|</span>
+            <ButtonSecondary>Login</ButtonSecondary>
+            <ButtonPrimary>Sign Up</ButtonPrimary>
+          </div>
+        </nav>
+      </header>
+
+      {/* Search Modal */}
+      <Modal searchModal={true} open={open} onClose={() => setOpen(false)}>
+        <SearchModalLayout searchInputRef={searchInputRef}/>
+      </Modal>
+    </>
   );
 };
 
