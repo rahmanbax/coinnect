bbf1f9a5b23dbf0308cbc7186db4cca48c72973c
rahmanbax <basyirahman@gmail.com>
2025-02-12 00:28:26 +0700

    Enhance SearchBar button with hover effects and cursor pointer for improved UX

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="flex items-center gap-1 px-2 py-1 bg-[#F5F5F5] rounded-full stroke-1 border-1 border-gray-200"
+      className="flex items-center gap-1 px-2 py-1 bg-[#F5F5F5] rounded-full stroke-1 border-1 border-gray-200 transition ease-out hover:shadow-sm hover:cursor-pointer"
     >
       <MagnifyingGlass className="opacity-50" size={12} weight="bold" />
       <p className="font-normal text-sm text-gray-500">Search</p>
