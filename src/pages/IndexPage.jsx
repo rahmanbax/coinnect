d141c3a5cdcd6f9b79ef1f55cfe41c335dfa48d8
rahmanbax <basyirahman@gmail.com>
2025-02-12 00:29:01 +0700

    Update IndexPage to display a placeholder message and link to the Market Page

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import NavBar from "../containers/NavBar";
+import ButtonPrimary from "../components/ButtonPrimary";
 import Footer from "../containers/Footer";
+import { Link } from "react-router-dom";
 
 const IndexPage = () => {
   return (
-    <div className="">
+    <div className="flex flex-col h-screen">
       <NavBar />
-      <div className="">Index</div>
+      <div className="flex flex-col items-center gap-4 justify-center flex-grow ">
+        <p className="font-medium text-2xl">
+          Sorry, this page hasn't finished yet!
+        </p>
+        <Link to={"/markets"}>
+          <ButtonPrimary>Go to Market Page</ButtonPrimary>
+        </Link>
+      </div>
+      <Footer />
     </div>
   );
 };
