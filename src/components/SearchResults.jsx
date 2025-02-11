28b99be20177fbaed9fd589dfe2b64a26b225b75
rahmanbax <basyirahman@gmail.com>
2025-02-11 20:28:55 +0700

    Replace SearchResultsCard with CoinCard in SearchResultsLayout component

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import PriceChange from "./PriceChange";
-import SearchResultsCard from "./SearchResultsCard";
+import CoinCard from "./CoinCard";
 
 const SearchResultsLayout = ({ searchData }) => {
   return (
     <>
       {/* Search results */}
       <p className="font-bold text-sm text-black/50">Search results</p>
-      {searchData.map((data) => <SearchResultsCard key={data.id} data={data} />)}
+      {searchData.map((data) => <CoinCard key={data.id} data={data} />)}
     </>
   );
 };
