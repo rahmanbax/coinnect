989d2c3ab3d1adc4f6dc2e73df4f6b918b43e4e7
rahmanbax <basyirahman@gmail.com>
2025-02-12 00:27:40 +0700

    Add cursor pointer to ButtonPrimary for better UX

--- src/components/ButtonPrimary.jsx
+++ src/components/ButtonPrimary.jsx
@@ -4,7 +4,7 @@ const ButtonPrimary = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="rounded-xl bg-[#FFC31B] hover:bg-[#E5AF18] text-black text-sm font-medium py-2 px-4 transition ease-out"
+      className="rounded-xl bg-[#FFC31B] cursor-pointer hover:bg-[#E5AF18] text-black text-sm font-medium py-2 px-4 transition ease-out"
     >
       {children}
     </button>
