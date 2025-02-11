f9f3e38c96b338feb8e33730a949a61a8318102e
rahmanbax <basyirahman@gmail.com>
2025-02-12 00:28:35 +0700

    Refactor TrendingNow component to utilize CoinCard for rendering trend data

--- src/components/TrendingNow.jsx
+++ src/components/TrendingNow.jsx
@@ -1,24 +1,12 @@
 import React from "react";
-import PriceChange from "./PriceChange";
+import CoinCard from "./CoinCard";
 
-const TrendingNowLayout = () => {
+const TrendingNowLayout = ({trendData}) => {
   return (
     <>
       {/* When not searching */}
       <p className="font-bold text-sm text-black/50">Trending now</p>
-      <div className="flex justify-between items-center p-3 mt-3 hover:bg-[#f5f5f5] transition ease-out rounded-xl">
-        <div className="flex gap-3 items-center">
-          <img className="rounded-full w-8 h-8" />
-          <div className="flex flex-col gap-1">
-            <p className="font-medium text-base">Bitcoin</p>
-            <p className="text-black/60 text-sm">BTC</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-1 items-end">
-          <p className="font-semibold text-base">$131,131,11</p>
-          <PriceChange priceChange={-0.5} textSizeBase={false} />
-        </div>
-      </div>
+      {trendData.map((data) => <CoinCard key={data.id} data={data} />)}
     </>
   );
 };
