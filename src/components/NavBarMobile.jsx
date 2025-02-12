ba4eb941cc91f34d565b047177b637364ed3862e
rahmanbax <basyirahman@gmail.com>
2025-02-13 01:01:08 +0700

    Add NavBarMobile component for mobile navigation and update NavBar for responsiveness

---
+++ src/components/NavBarMobile.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import coinnect from "../assets/coinnect-pictorial-logo.png";
+import { GithubLogo, X } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
+
+const NavBarMobile = ({ open, onClose }) => {
+  if (!open) return null;
+
+  return (
+    <div
+      className={`absolute size-full ${
+        open ? "visible bg-white backdrop-blur-xs" : "invisible"
+      }
+      
+      `}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="my-5 flex flex-col gap-5"
+      >
+        <div className="flex mx-5 justify-between">
+          <img src={coinnect} alt="" className="h-5" />
+          <button onClick={onClose} className="cursor-pointer">
+            <X size={20} weight="bold" />
+          </button>
+        </div>
+        <Link to={"/markets"} className="p-5 hover:bg-[#f5f5f5] w-full font-medium text-2xl">
+          Markets
+        </Link>
+        <a
+          href="https://github.com/rahmanbax/crypto-app"
+          target="blank"
+          className="absolute bottom-5 left-5 flex items-center gap-2"
+        >
+          <GithubLogo className="text-base" weight="fill" />
+          <p className="text-sm font-medium">Source Code</p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBarMobile;
--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -4,10 +4,12 @@ import SearchBar from "../components/SearchBar";
 import { Link } from "react-router-dom";
 import Modal from "../components/Modal";
 import SearchModalLayout from "../components/SearchModal";
-import { GithubLogo } from "@phosphor-icons/react";
+import { GithubLogo, List } from "@phosphor-icons/react";
+import NavBarMobile from "../components/NavBarMobile";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
+  const [openNavBar, setOpenNavBar] = useState(false);
   const searchInputRef = useRef(null);
 
   useEffect(() => {
@@ -17,52 +19,51 @@ const NavBar = () => {
     } else {
       document.body.style.overflow = "auto";
     }
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
   }, [open]);
 
   return (
     <>
-      <header className="bg-white p-4">
-        <nav className="flex justify-between items-center mx-14">
+      <header className="bg-white py-4">
+        <nav className="flex justify-between items-center mx-5 lg:mx-14">
           <div className="flex gap-10 items-center">
             <Link to="/">
-              <img src={CoinnectLogo} alt="" className="w-[119px] h-[25px]" />
+              <img src={CoinnectLogo} alt="" className="h-6" />
             </Link>
-            <Link className="text-sm font-medium" to="/markets">
+            <Link className="text-sm font-medium hidden lg:block" to="/markets">
               Markets
             </Link>
-            {/* <Link className="text-sm font-medium" to="/swap">
-              Swap
-            </Link>
-            <Link className="text-sm font-medium" to="/tracker">
-              Portfolio Tracker
-            </Link> */}
           </div>
-          <div className="flex items-center gap-8">
+          <div className="flex items-center gap-5 lg:gap-8">
             <SearchBar onClick={() => setOpen(true)} />
-            <span className="text-sm opacity-20">|</span>
+            <span className="text-sm opacity-20 hidden lg:block">|</span>
             <a
               href="https://github.com/rahmanbax/crypto-app"
               target="blank"
-              className="flex gap-1 text-center"
+              className="gap-1 text-center hidden lg:flex"
             >
               <GithubLogo className="text-base" weight="fill" />
               <p className="text-sm font-medium">GitHub</p>
             </a>
 
-            {/* <ButtonSecondary>Login</ButtonSecondary>
-            <ButtonPrimary>Sign Up</ButtonPrimary> */}
+            <button
+              className="display lg:hidden cursor-pointer"
+              onClick={() => setOpenNavBar(true)}
+            >
+              <List className="" size={24} fill="bold" />
+            </button>
           </div>
         </nav>
       </header>
 
       {/* Search Modal */}
       <Modal searchModal={true} open={open} onClose={() => setOpen(false)}>
-        <SearchModalLayout searchInputRef={searchInputRef} />
+        <SearchModalLayout searchInputRef={searchInputRef} closeButton={() => setOpen(false)}/>
       </Modal>
+      <NavBarMobile open={openNavBar} onClose={() => setOpenNavBar(false)}>
+        <div className="bg-white size-full">
+          Test
+        </div>
+      </NavBarMobile>
     </>
   );
 };
