b51009512c7da992a14b9299626af1ffc94e803e
rahmanbax <basyirahman@gmail.com>
2025-02-13 01:00:38 +0700

    Refactor CryptoTable components for improved layout, text sizing, and responsiveness

--- src/components/CryptoTableBody.jsx
+++ src/components/CryptoTableBody.jsx
@@ -5,18 +5,18 @@ import { formatNumberSeparatedWithComma, formatNumberToString } from "../utils/f
 const CryptoTableBody = ({ coinData }) => {
   return (
     <div className="bg-white hover:bg-gray-50 flex text-base items-center border-t-1 border-[#f5f5f5] transition ease-out">
-      <div className="text-center w-[59px] p-5">{coinData.rank}</div>
-      <div className="text-center w-20 p-5">
+      <div className="text-center w-[49px] md:w-[59px] p-4 md:p-5 text-xs md:text-sm">{coinData.rank}</div>
+      <div className="text-center w-11 md:w-20 flex justify-center">
         <img
           src={coinData.icon}
           alt={coinData.symbol}
-          className="w-10 h-10 rounded-full"
+          className="w-7 md:w-10 rounded-full"
         />
       </div>
-      <div className="p-5 flex-1 flex flex-col gap-1">
-        <p className="w-full">{coinData.name}</p>
+      <div className="p-4 md:p-5 flex-1 flex flex-col gap-1">
+        <p className="w-full text-sm md:text-base">{coinData.name}</p>
         <span className="text-black/60 hidden md:block text-sm">{coinData.symbol}</span>
-        <span className="text-black/80 md:hidden text-sm">${formatNumberSeparatedWithComma(coinData.price)}</span>
+        <span className="text-black/80 md:hidden text-xs md:text-sm">${formatNumberSeparatedWithComma(coinData.price)}</span>
       </div>
       <div className="p-5 w-[116px] hidden xl:block ">
         <PriceChange priceChange={coinData.priceChange1h} />
--- src/components/CryptoTableHeader.jsx
+++ src/components/CryptoTableHeader.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const CryptoTableHeader = () => {
   return (
-    <div className="bg-white text-black/60 flex w-full items-center h-[58px] text-sm">
-      <div className="text-center w-[59px] p-5">#</div>
-      <div className="text-center w-20 p-5"></div>
-      <div className="p-5 flex-1">Name</div>
-      <div className="p-5 w-[116px] hidden xl:block text-end">1h</div>
-      <div className="p-5 w-[116px] text-end">24h</div>
-      <div className="p-5 w-[116px] hidden lg:block text-end">7d</div>
-      <div className="p-5 w-[160px] hidden md:block text-end">Price</div>
-      <div className="p-5 w-[132px] hidden xl:block text-end">Market Cap</div>
-      <div className="p-5 w-[148px] hidden lg:block text-end">Volume 24h</div>
+    <div className="bg-white text-black/60 flex w-full items-center text-xs md:text-sm">
+      <div className="text-center w-[49px] md:w-[59px] p-4 md:p-5">#</div>
+      <div className="text-center w-11 md:w-20 p-4 md:p-5"></div>
+      <div className="p-4 md:p-5 flex-1">Name</div>
+      <div className="p-4 md:p-5 w-[116px] hidden xl:block text-end">1h</div>
+      <div className="p-4 md:p-5 w-[116px] text-end">24h</div>
+      <div className="p-4 md:p-5 w-[116px] hidden lg:block text-end">7d</div>
+      <div className="p-4 md:p-5 w-[160px] hidden md:block text-end">Price</div>
+      <div className="p-4 md:p-5 w-[132px] hidden xl:block text-end">Market Cap</div>
+      <div className="p-4 md:p-5 w-[148px] hidden lg:block text-end">Volume 24h</div>
     </div>
   );
 };
