5fddd0876d9572f7669f19c15034e9f1a9932457
rahmanbax <basyirahman@gmail.com>
2025-02-13 01:00:29 +0700

    Refactor CoinCard component for improved layout and text sizing

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -4,16 +4,16 @@ import PriceChange from "./PriceChange";
 
 const CoinCard = ({ data }) => {
   return (
-    <div className="flex justify-between items-center p-3 mt-3 hover:bg-[#f5f5f5] transition ease-out rounded-xl">
+    <div className="flex justify-between items-center p-3 mt-3 hover:bg-[#f5f5f5] w-full transition ease-out rounded-xl">
       <div className="flex gap-3 items-center">
-        <img src={data.icon} alt={data.name} className="rounded-full w-8 h-8" />
-        <div className="flex flex-col gap-1">
-          <p className="font-medium text-base">{data.name}</p>
-          <p className="text-black/60 text-sm">{data.symbol}</p>
+        <img src={data.icon} alt={data.name} className="rounded-full w-6 h-6 md:w-8 md:h-8" />
+        <div className="flex flex-1 flex-col gap-1">
+          <p className="font-medium text-sm md:text-base">{data.name}</p>
+          <p className="text-black/60 text-xs md:text-sm">{data.symbol}</p>
         </div>
       </div>
       <div className="flex flex-col gap-1 items-end">
-        <p className="font-semibold text-base">
+        <p className="font-semibold text-sm md:text-base">
           ${formatNumberSeparatedWithComma(data.price)}
         </p>
         <PriceChange priceChange={data.priceChange1d} textSizeBase={false} />
