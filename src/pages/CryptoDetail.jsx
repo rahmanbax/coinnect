1cbbb562a943322dbab91a3f063a50582fd6e160
rahmanbax <basyirahman@gmail.com>
2025-02-17 02:20:27 +0700

    Refactor CryptoDetail component to update import paths and add StatsCard for displaying market data

--- src/pages/CryptoDetail.jsx
+++ src/pages/CryptoDetail.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import TradingViewWidget from "../containers/TradingViewWidget";
+import TradingViewWidget from "../components/TradingViewWidget";
 import { useParams } from "react-router-dom";
 import { getCoinsDetail } from "../services/product.service";
 import NavBar from "../containers/NavBar";
 import CryptoDetailHeader from "../components/CryptoDetail/CryptoDetailHeader";
-import CryptoDetailBreadcrumb from "../components/CryptoDetail/CryptoDetailBreadcrumb";
-import PriceChange from "../components/PriceChange";
-import { formatNumberSeparatedWithComma } from "../utils/formatNumber";
+import CryptoDetailBreadcrumb from "../components/CryptoDetail/Breadcrumb";
 import CryptoDetailPrice from "../components/CryptoDetail/CryptoDetailPrice";
+import StatsCard from "../components/CryptoDetail/StatsCard";
+import {
+  formatNumber,
+  formatNumberSeparatedWithComma,
+} from "../utils/formatNumber";
+import Footer from "../containers/Footer";
 
 const CryptoDetail = () => {
   const { id } = useParams();
@@ -30,19 +34,44 @@ const CryptoDetail = () => {
   return (
     <>
       <NavBar />
-      <main className="my-5 mx-5">
+      <main className="my-5 mx-5 md:mx-10 lg:mx-auto lg:w-[984px] xl:w-[1280px]">
         <CryptoDetailBreadcrumb data={cryptoDetails} />
         <CryptoDetailHeader data={cryptoDetails} />
-        <CryptoDetailPrice data={cryptoDetails}/>
+        <CryptoDetailPrice data={cryptoDetails} />
         <div className="mt-5 h-[400px] md:h-[500px]">
-          {cryptoDetails ? (
-            <TradingViewWidget key={id} id={id} cryptoDetails={cryptoDetails} />
-          ) : (
-            <div>Loading charts...</div>
+          {cryptoDetails && (
+            <TradingViewWidget
+              key={cryptoDetails.id}
+              id={id}
+              cryptoDetails={cryptoDetails}
+            />
           )}
         </div>
 
+        {cryptoDetails && (
+          <div className="mt-5 grid grid-cols-2 gap-4 xl:grid-cols-4">
+            <StatsCard
+              title={"Market Cap"}
+              data={`$${formatNumberSeparatedWithComma(
+                cryptoDetails?.marketCap
+              )}`}
+            />
+            <StatsCard
+              title={"Volume"}
+              data={`$${formatNumberSeparatedWithComma(cryptoDetails?.volume)}`}
+            />
+            <StatsCard
+              title={"Total Supply"}
+              data={`${formatNumber(cryptoDetails?.totalSupply)}`}
+            />
+            <StatsCard
+              title={"Available Supply"}
+              data={`${formatNumber(cryptoDetails?.availableSupply)}`}
+            />
+          </div>
+        )}
       </main>
+      <Footer />
     </>
   );
 };
